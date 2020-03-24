f79eb4182b6cccfa925a4b64a59f1b5c01d506aa
Fede <fedegiobergia@gmail.com>
2020-03-24 16:13:22 -0300

    Fix flex

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header/>
       <div
         style={{
           margin: `0 auto`,
@@ -36,8 +36,7 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer class="container">
           © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          {` ❤️ and ☕ by Fede`}
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,24 @@ const IndexPage = () => (
     <div class="container">
       <div class="content">
         <SEO title="Home" />
-        <h1> <span class="highlight">Fede Giobergia</span></h1>
-        <p>Nostrud anim occaecat sunt anim tempor adipisicing cillum in laboris. Nulla aute commodo occaecat nisi commodo anim adipisicing. Proident do ullamco excepteur laborum qui veniam consectetur. Fugiat nulla consequat ullamco eu. Nisi anim esse et ipsum in dolor aute nulla elit ex deserunt. Irure nisi ex cupidatat velit aute excepteur fugiat cillum veniam id eu voluptate. Amet proident occaecat cillum fugiat eiusmod laborum irure velit minim occaecat nulla anim.</p>
-        <p>Now go build something great.</p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <Image />
         </div>
-        <Link to="/page-2/">Go to page 2</Link>
+        <h1> <span class="highlight">Fede Giobergia</span></h1>
+        <p>Nostrud anim occaecat sunt anim tempor adipisicing cillum in laboris. Nulla aute commodo occaecat nisi commodo anim adipisicing. Proident do ullamco excepteur laborum qui veniam consectetur. Fugiat nulla consequat ullamco eu. Nisi anim esse et ipsum in dolor aute nulla elit ex deserunt. Irure nisi ex cupidatat velit aute excepteur fugiat cillum veniam id eu voluptate. Amet proident occaecat cillum fugiat eiusmod laborum irure velit minim occaecat nulla anim.</p>
+        <p>Now go build something great.</p>
+
+
+        <div class="card">
+          <div class="container-card">
+            <div style={{borderRadius: `15px`}}>
+              <Image />
+            </div>
+            <h4><b>Sarasa</b></h4>
+          </div>
+        </div>
+
+        {/* <Link to="/page-2/">Go to page 2</Link> */}
       </div>
     </div>
   </Layout>
