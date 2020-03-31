39d1287c6f21b9aacb95b82729f2ce16d5f506a7
Fede <fedegiobergia@gmail.com>
2020-03-31 03:27:42 -0300

    fix seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,27 +10,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-162140742-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        trackingId: "UA-162140742-1"
       },
     },
     `gatsby-plugin-sass`,
@@ -59,7 +39,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon:`static/favicon.png`, // This path is relative to the root of the site.
+        icon:`src/static/favicon.png`, // This path is relative to the root of the site.
       },
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
